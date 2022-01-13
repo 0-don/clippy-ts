cd1b674bcc092756439ad7d2f3b170db3e898d1a
Don-Cryptus <don.cryptus@gmail.com>
2022-01-14 00:51:19 +0100

    fix debian binaries

--- beforePackHook.js
+++ beforePackHook.js
@@ -3,31 +3,30 @@ const path = require('path');
 
 // (context) => void
 exports.default = () => {
-  const migrationBinaryLocations = './node_modules/@prisma/engines/';
-  const queryBinaryLocations = './node_modules/.prisma/client/';
+  const binariesLocation = './node_modules/@prisma/engines/';
 
   const binaries = [
     {
-      migration: `${migrationBinaryLocations}migration-engine-windows.exe`,
-      query: `${queryBinaryLocations}query_engine-windows.dll.node`,
+      migration: `migration-engine-windows.exe`,
+      query: `query_engine-windows.dll.node`,
       os: 'win',
     },
     {
-      migration: `${migrationBinaryLocations}migration-engine-darwin`,
-      query: `${queryBinaryLocations}libquery_engine-darwin.dylib.node`,
+      migration: `migration-engine-darwin`,
+      query: `libquery_engine-darwin.dylib.node`,
       os: 'mac',
     },
     {
-      migration: `${migrationBinaryLocations}migration-engine-rhel-openssl-1.1.x`,
-      query: `${queryBinaryLocations}libquery_engine-debian-openssl-1.1.x.so.node`,
+      migration: `migration-engine-debian-openssl-1.1.x`,
+      query: `libquery_engine-debian-openssl-1.1.x.so.node`,
       os: 'linux',
     },
   ];
 
   binaries.forEach(({ migration, os, query }) => {
-    const dest = path.join(queryBinaryLocations, os);
-    const queryDest = path.join(dest, path.basename(query));
-    const migrationDest = path.join(dest, path.basename(migration));
+    const dest = path.join('./node_modules/.prisma/client/', os);
+    const queryDest = path.join(binariesLocation, query);
+    const migrationDest = path.join(binariesLocation, migration);
 
     if (!fs.existsSync(dest)) fs.mkdirSync(dest);
 
