ffe12f7b2582118a610b1225d05ff21773ded747
Don-Cryptus <don.cryptus@gmail.com>
2022-01-13 01:53:47 +0100

    fix

--- myBeforePackHook.js
+++ myBeforePackHook.js
@@ -8,11 +8,11 @@ exports.default = (context) => {
   log('################');
   fs.copyFileSync(
     './node_modules/.prisma/client/query_engine-windows.dll.node',
-    './platforms/win32/query_engine-windows.dll.node'
+    './platforms/win/query_engine-windows.dll.node'
   );
   fs.copyFileSync(
     './node_modules/.prisma/client/libquery_engine-darwin.dylib.node',
-    './platforms/darwin/libquery_engine-darwin.dylib.node'
+    './platforms/mac/libquery_engine-darwin.dylib.node'
   );
   fs.copyFileSync(
     './node_modules/.prisma/client/libquery_engine-debian-openssl-1.1.x.so.node',
