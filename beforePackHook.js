7fdfc07da05ce11336f572fabe6cc0e62630ec40
Don-Cryptus <don.cryptus@gmail.com>
2022-01-14 00:04:23 +0100

    migration binary

--- beforePackHook.js
+++ beforePackHook.js
@@ -3,42 +3,35 @@ const path = require('path');
 
 // (context) => void
 exports.default = () => {
-  const quueryBinaryLocations = './node_modules/.prisma/client';
+  const migrationBinaryLocations = './node_modules/@prisma/engines/';
+  const queryBinaryLocations = './node_modules/.prisma/client/';
 
-  const queryBinaries = [
-    { filename: 'query_engine-windows.dll.node', os: 'win' },
-    { filename: 'libquery_engine-darwin.dylib.node', os: 'mac' },
-    { filename: 'libquery_engine-debian-openssl-1.1.x.so.node', os: 'linux' },
+  const binaries = [
+    {
+      migration: `${migrationBinaryLocations}migration-engine-windows.exe`,
+      query: `${queryBinaryLocations}query_engine-windows.dll.node`,
+      os: 'win',
+    },
+    {
+      migration: `${migrationBinaryLocations}migration-engine-darwin`,
+      query: `${queryBinaryLocations}libquery_engine-darwin.dylib.node`,
+      os: 'mac',
+    },
+    {
+      migration: `${migrationBinaryLocations}migration-engine-rhel-openssl-1.1.x`,
+      query: `${queryBinaryLocations}libquery_engine-debian-openssl-1.1.x.so.node`,
+      os: 'linux',
+    },
   ];
 
-  queryBinaries.forEach(({ filename, os }) => {
-    const binSourceLocation = `${quueryBinaryLocations}/${filename}`;
-    const binDestLocation = `${quueryBinaryLocations}/${os}/${filename}`;
+  binaries.forEach(({ migration, os, query }) => {
+    const dest = path.join(queryBinaryLocations, os);
+    const queryDest = path.join(dest, path.basename(query));
+    const migrationDest = path.join(dest, path.basename(migration));
 
-    if (!fs.existsSync(path.dirname(binDestLocation)))
-      fs.mkdirSync(path.dirname(binDestLocation));
+    if (!fs.existsSync(dest)) fs.mkdirSync(dest);
 
-    fs.copyFileSync(binSourceLocation, binDestLocation);
+    fs.copyFileSync(query, queryDest);
+    fs.copyFileSync(migration, migrationDest);
   });
-
-  // const migrationBinaries = [
-  //   {
-  //     gzName: 'migration-engine.exe.gz',
-  //     filename: 'migration-engine.exe',
-  //     url: 'https://binaries.prisma.sh/2.1.x/latest/windows/migration-engine.exe.gz',
-  //     os: 'win',
-  //   },
-  //   {
-  //     gzName: 'migration-engine.exe.gz',
-  //     filename: 'migration-engine',
-  //     url: 'https://binaries.prisma.sh/2.10.x/2b17eeb22ae07bb9df44af54cb39f2db709b6a84/darwin/migration-engine.gz',
-  //     os: 'mac',
-  //   },
-  //   {
-  //     gzName: 'migration-engine.exe.gz',
-  //     filename: 'migration-engine.exe',
-  //     url: 'https://binaries.prisma.sh/2.1.x/latest/darwin/migration-engine.gz',
-  //     os: 'linux',
-  //   },
-  // ];
 };
